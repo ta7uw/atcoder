0db8e255daf84912e47f7a112fbd78a206c1b8a5
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-16 17:26:41 +0900

    Add: CalculateNAVTest

--- src/com/functionalprograming/chapter4/CalculateNAVTest.java
+++ src/com/functionalprograming/chapter4/CalculateNAVTest.java
@@ -1,9 +1,16 @@
+package com.functionalprograming.chapter4;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.math.BigDecimal;
+import com.functionalprograming.chapter4.CalculateNAV.*;
 
 import static org.junit.Assert.*;
 
@@ -12,8 +19,17 @@ public class CalculateNAVTest {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addClass(com.functionalprograming.chapter4.CalculateNAV.class)
+                .addClass(CalculateNAV.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
+    @Test
+    public void computeStockWorth() {
+        final CalculateNAV calculateNav = new CalculateNAV(ticker->new BigDecimal("6.01"));
+        BigDecimal expected = new BigDecimal("6010.00");
+        Assert.assertEquals(0,
+                calculateNav.computeStockWorth("GOOG", 1000).compareTo(expected),
+                0.001);
+
+    }
 }
