ae6649dcad16056d66bd6a42a21437e1682bcd40
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-16 16:31:55 +0900

    Modify: add Predicate argument to AssetUtils

--- src/com/functionalprograming/chapter4/AssetUtil.java
+++ src/com/functionalprograming/chapter4/AssetUtil.java
@@ -1,10 +1,13 @@
 package com.functionalprograming.chapter4;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AssetUtil {
-    public static int totalAssetValues(final List<Asset> assets){
+    public static int totalAssetValues(final List<Asset> assets,
+                                       final Predicate<Asset> assetSelector){
         return assets.stream()
+                        .filter(assetSelector)
                         .mapToInt(Asset::getValue)
                         .sum();
     }
