bc15a5ca0cc4e6a86c995de2b08cf8ea3784eef8
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-21 22:56:02 +0900

    Add: Practice3

---
+++ src/AtCoder/Practice3.java
@@ -0,0 +1,19 @@
+package AtCoder;
+
+import java.util.Scanner;
+
+public class Practice3 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int count = 0;
+        for (int i =0; i<3;i++){
+            String num =line.substring(i,i+1);
+            if (num.equals("1")){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
