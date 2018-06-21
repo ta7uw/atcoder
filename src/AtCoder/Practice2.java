721beebdda66e6274eb891df2b306b3a8c28cea6
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-21 22:43:08 +0900

    Add: Practice2

---
+++ src/AtCoder/Practice2.java
@@ -0,0 +1,13 @@
+package AtCoder;
+
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] atgs){
+        Scanner sc = new Scanner(System.in);
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+        if (firstNum * secondNum % 2==0) System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
