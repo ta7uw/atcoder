c1eb46bc215b0d3a6215f7b972c6d38067064ceb
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-21 22:25:56 +0900

    Add: Practice1

---
+++ src/AtCoder/Practice1.java
@@ -0,0 +1,16 @@
+package AtCoder;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String firstNum = scanner.nextLine();
+        String secondNum = scanner.next();
+        String thirdNum = scanner.nextLine().replace(" ", "");
+        String str = scanner.nextLine();
+        int sum = Integer.valueOf(firstNum) + Integer.valueOf(secondNum) + Integer.valueOf(thirdNum);
+        System.out.println(sum +" "+ str);
+
+    }
+}
