a4ed3d823c9c1c0e2dc8301a89702e4ca140ac65
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-30 19:11:27 +0900

    Create: main format

---
+++ src/AtCoder/Main.java
@@ -0,0 +1,12 @@
+package AtCoder;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        out.flush();
+    }
+}
