38436c54b1b770c493f545fe3e10917630010d2a
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-06-30 19:05:46 +0900

    Prepare: next contest

--- src/AtCoder/ABC102/Q1/Main.java
+++ src/AtCoder/ABC102/Q1/Main.java
@@ -1,4 +1,13 @@
 package AtCoder.ABC102.Q1;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        out.flush();
+    }
 }
+
--- src/AtCoder/ABC102/Q4/Main.java
+++ src/AtCoder/ABC102/Q4/Main.java
@@ -1,4 +1,12 @@
 package AtCoder.ABC102.Q4;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        out.flush();
+    }
 }
