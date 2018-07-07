73aad58c2a7ef5d4d98781a80aa63e0b36276fbb
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-07 20:52:10 +0900

    Prepare: SPC

---
+++ src/AtCoder/SPC/Q5/Mian.java
@@ -0,0 +1,50 @@
+package AtCoder.SPC.Q5;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        out.flush();
+        sc.close();
+    }
+
+    static int nint(Scanner sc){
+        return Integer.parseInt(sc.next());
+    }
+    static long nlong(Scanner sc){
+        return Long.parseLong(sc.next());
+    }
+    static double ndouble(Scanner sc){
+        return Double.parseDouble(sc.next());
+    }
+    static float nfloat(Scanner sc){
+        return Float.parseFloat(sc.next());
+    }
+    static String nstr(Scanner sc){
+        return  String.valueOf(sc.next());
+    }
+    static long[] longLine(Scanner sc, int size){
+        long[] lLine = new long[size];
+        for (int i = 0; i < size; i++) {
+            lLine[i] = nlong(sc);
+        }
+        return lLine;
+    }
+    static int[] intLine(Scanner sc, int size){
+        int[] iLine = new int[size];
+        for (int i = 0; i < size; i++) {
+            iLine[i] = nint(sc);
+        }
+        return iLine;
+    }
+    static String[] strLine(Scanner sc, int size){
+        String[] strLine = new String[size];
+        for (int i = 0; i < size; i++) {
+            strLine[i] = nstr(sc);
+        }
+        return strLine;
+    }
+}
