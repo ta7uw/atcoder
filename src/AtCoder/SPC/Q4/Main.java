46b42153d84f668d2c7c01c75121ba803d3be325
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-08 21:48:49 +0900

    Implement: Dijkstra

--- src/AtCoder/SPC/Q4/Main.java
+++ src/AtCoder/SPC/Q4/Main.java
@@ -15,28 +15,96 @@ public class Main {
         out.flush();
         sc.close();
     }
-    static class Task{
+    private static class Task{
         public void solve(Scanner sc, PrintWriter out){
             double startYen = Math.pow(10,15);
             int n = nint(sc);
             int m = nint(sc);
             int s = nint(sc);
             int t = nint(sc);
-            List<List<Long>> fares = new ArrayList<>();
+            Dijkstra Di = new Dijkstra();
+            Di.execute(sc, n, m, s, t);
 
-            for(int i=0; i<m; i++){
-                List<Long> fare= new ArrayList<>();
-                fare.add(nlong(sc));
-                fare.add(nlong(sc));
-                fare.add(nlong(sc));
-                fare.add(nlong(sc));
-                fares.add(fare);
-            }
-            int maxYear = n-1;
-            boolean changeToSunuke = false;
-            for (int year=0; year<=maxYear; year++){
+        }
+    }
 
+    public static class Dijkstra{
+        public void execute(Scanner sc, int numOfCity, int numOfRoute,
+                            int startCity, int dstCity){
+            // 都市と道路の接続関係を示すマップ
+            int[][] map = new int[numOfCity][numOfCity];
+            //　マップの初期化
+            for(int i=0; i<numOfCity; i++){
+                for(int j=0; j< numOfCity; j++){
+                    map[i][j] = (i==j) ? 0:-1;
+                }
             }
+            //　道路状況の読み込み
+            for (int i=0; i<numOfRoute; i++){
+                int from = nint(sc);
+                int to = nint(sc);
+                int length = nint(sc);
+                map[from][to] = length;
+                map[to][from] = length;
+            }
+
+            // 各都市までの最短距離
+            int[] distance = new int[numOfCity];
+            dijkstra(map, startCity, distance);
+            if (distance[dstCity]==Integer.MAX_VALUE) {	// 解なし
+                System.out.println("no route");
+            } else {
+                System.out.println("distance="+distance[dstCity]);
+            }
+
+        }
+
+        private void dijkstra(int[][] map, int stratCity, int[] distance){
+            int numOfCity = distance.length;
+            // 最短距離の確定したか
+            boolean[] isFixed = new boolean[numOfCity];
+
+            for (int i=0; i<numOfCity; i++){
+                distance[i] = Integer.MAX_VALUE;
+                isFixed[i] = false;
+            }
+
+            // 出発点までの距離を０とする
+            distance[0] = 0;
+            while (true){
+                // 未確定の中で最も近い都市を求める
+                int marked = minindex(distance, isFixed);
+                // 全都市が確定した場合
+                if (marked < 0) return;
+                // 非連結グラフ
+                if (distance[marked] == Integer.MAX_VALUE) return;
+                // その都市までの最短距離は確定
+                isFixed[marked] = true;
+                // 隣の都市
+                for (int i=0; i<numOfCity;i++){
+                    // 未確定ならば
+                    if (map[marked][i] > 0 && !isFixed[i]){
+                        int newDistance = distance[marked] + map[marked][i];
+                        //　今までのよりも小さければ、それを覚える
+                        if (newDistance < distance[i]) distance[i] = newDistance;
+                    }
+                }
+            }
+        }
+
+        private  int minindex(int[] distance, boolean[] isFixed){
+            int index = 0;
+            // 未確定の都市をどれかさがす
+            for(;index < isFixed.length; index++){
+                if (!isFixed[index]) break;
+            }
+            // 未確定の都市が存在しなければ-1
+            if (index == isFixed.length) return -1;
+            // 距離が小さいものを探す
+            for (int i = index + 1; i<isFixed.length;i++){
+                if (!isFixed[i] && distance[i] < distance[index]) index =i;
+            }
+            return index;
         }
     }
 
