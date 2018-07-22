6a79a8a72bd9b6cb8efb4572bac12fd72de8096e
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-22 15:42:17 +0900

    Implement: practice8 in beginners selection

---
+++ src/AtCoder/BeginnersSelection/Practice8.java
@@ -0,0 +1,124 @@
+package AtCoder.BeginnersSelection;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Practice8 {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        Task task = new Task();
+        task.solve(sc, out);
+        out.flush();
+        sc.close();
+    }
+
+    static class Task {
+        public void solve(Scanner sc, PrintWriter out) {
+            int N = nint(sc);
+            List<Integer> list = getIntegerList(sc, N);
+            list = list.stream().sorted(Comparator.reverseOrder())
+                    .collect(Collectors.toList());
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (i == 0) {
+                    count++;
+                } else if (list.get(i - 1) > list.get(i)) {
+                    count++;
+
+                }
+            }
+            out.println(count);
+        }
+    }
+
+    static int nint(Scanner sc) {
+        return Integer.parseInt(sc.next());
+    }
+
+    static long nlong(Scanner sc) {
+        return Long.parseLong(sc.next());
+    }
+
+    static double ndouble(Scanner sc) {
+        return Double.parseDouble(sc.next());
+    }
+
+    static float nfloat(Scanner sc) {
+        return Float.parseFloat(sc.next());
+    }
+
+    static String nstr(Scanner sc) {
+        return String.valueOf(sc.next());
+    }
+
+    static long[] longLine(Scanner sc, int size) {
+        long[] lLine = new long[size];
+        for (int i = 0; i < size; i++) {
+            lLine[i] = nlong(sc);
+        }
+        return lLine;
+    }
+
+    static int[] intLine(Scanner sc, int size) {
+        int[] iLine = new int[size];
+        for (int i = 0; i < size; i++) {
+            iLine[i] = nint(sc);
+        }
+        return iLine;
+    }
+
+    static String[] strLine(Scanner sc, int size) {
+        String[] strLine = new String[size];
+        for (int i = 0; i < size; i++) {
+            strLine[i] = nstr(sc);
+        }
+        return strLine;
+    }
+
+    static long maxFromList(List<Long> longList) {
+        return longList.stream().max(Comparator.naturalOrder()).get();
+    }
+
+    static long minFromList(List<Long> longList) {
+        return longList.stream().min(Comparator.naturalOrder()).get();
+    }
+
+    public static int sumDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static long sumDigits(long n) {
+        long sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    static List<Integer> getIntegerList(Scanner sc, int size) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(nint(sc));
+        }
+        return list;
+    }
+
+    static List<Long> getLongList(Scanner sc, int size) {
+        List<Long> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(nlong(sc));
+        }
+        return list;
+    }
+}
