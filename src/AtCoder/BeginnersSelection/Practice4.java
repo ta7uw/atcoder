33a27c88f263b5b46596aa68c60f4ccc3f48a9b9
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-07-22 14:01:58 +0900

    Implement: practice4 in beginners selection

--- src/AtCoder/BeginnersSelection/Practice4.java
+++ src/AtCoder/BeginnersSelection/Practice4.java
@@ -1,24 +1,28 @@
 package AtCoder.BeginnersSelection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Practice4 {
     public static void  main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Integer[] array = new Integer[N];
+        List<Long> inputList = new ArrayList<>();
         for (int i=0; i<N; i++){
-            Integer num = sc.nextInt();
-            array[i] = num;
+            long num = sc.nextLong();
+            inputList.add(num);
         }
-        int count = 0;
-        Stream<Integer> stream = Arrays.asList(array).stream();
+        long count = 0;
+        Stream<Long> stream = inputList.stream();
         while (stream.filter(s-> s%2 ==0).count() == N){
             count ++;
-            stream = stream.map(s-> s/2);
-            stream.forEach(System.out::println);
+            final long tmp = (long)Math.pow(2, count);
+            List<Long> newList = inputList.stream().map(s -> s/tmp).collect(Collectors.toList());
+            stream = newList.stream();
         }
         System.out.println(count);
     }
