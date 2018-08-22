a0623ed00e464a327b81f3d3daff10448b237eb6
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-08-22 22:48:13 +0900

    ABC087 B

--- src/AtCoder/ABC087/B.py
+++ src/AtCoder/ABC087/B.py
@@ -1,2 +1,12 @@
-N , K = map(int, input().split())
-print((K-1)**(N-1) *K)
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())
+count = 0
+for a in range(A+1):
+    for b in range(B+1):
+        for c in range(C+1):
+            if 500*a + 100*b + 50*c == X:
+                count +=1
+print(count)
+
