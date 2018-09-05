f01ed9113edad55a9188166efde4ad26e0e5cc69
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-09-05 09:02:13 +0900

    ABC080 A

--- src/AtCoder/ABC080/A.py
+++ src/AtCoder/ABC080/A.py
@@ -1,21 +1,3 @@
-N = int(input())
-f_list = []
-p_list = []
+N, A, B = map(int, input().split())
 
-for i in range(N):
-    f = input()
-    f_list.append(f)
-for i in range(N):
-    p = list(map(int, input().split()))
-    p_list.append(p)
-
-profit = []
-for i in range(1, 2**10):
-    sum = 0
-    for f, p in zip(f_list, p_list):
-        f = int(f.replace(" ", ""), 2)
-        c = bin(f & i).count("1")
-        sum += p[c]
-    profit.append(sum)
-print(max(profit))
-
+print(min(A*N, B))
