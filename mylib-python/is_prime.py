8b48aa831000dcd3f0fee3f89a8f0ef0183d699f
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-10-07 17:27:40 +0900

    Add is_prime in mylib-python

---
+++ mylib-python/is_prime.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    """
+    This function checks whether the input positive integer is a prime number.
+    :param num:
+    :return: boolean
+    """
+    i = 2
+    while i ** 2 <= num:
+        if num % i == 0:
+            return False
+        i += 1
+    return True
