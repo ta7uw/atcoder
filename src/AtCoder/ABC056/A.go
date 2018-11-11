0f59db46f3da1a19ca6c003b57aced2dc1af010c
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-11 09:44:01 +0900

    ABC056 A

---
+++ src/AtCoder/ABC056/A.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b string
+	fmt.Scan(&a, &b)
+
+	if a == "H" && b=="H"{
+		fmt.Println("H")
+	}else if a=="D" && b=="D"{
+		fmt.Println("H")
+	}else{
+		fmt.Println("D")
+	}
+
+}
+
+
