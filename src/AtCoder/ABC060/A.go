8ab542ff143f19941bb280c66b26ed86b5b6567e
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-11 10:36:45 +0900

    ABC060 A

---
+++ src/AtCoder/ABC060/A.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var A, B, C string
+	fmt.Scan(&A, &B, &C)
+
+	if A[len(A)-1] == B[0] && B[len(B)-1] == C[0]{
+		fmt.Println("YES")
+	}else{
+		fmt.Println("NO")
+	}
+
+}
