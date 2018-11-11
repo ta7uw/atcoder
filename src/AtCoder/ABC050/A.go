7b4977c5e82a7e69235eba5c9ab900f8b2da5984
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-11 10:28:35 +0900

    ABC050 A

---
+++ src/AtCoder/ABC050/A.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a, b int
+	var op string
+	fmt.Scan(&a)
+	fmt.Scan(&op)
+	fmt.Scan(&b)
+	
+	if op == "+"{
+		fmt.Println(a+b)
+	}else {
+		fmt.Println(a-b)
+	}
+}
