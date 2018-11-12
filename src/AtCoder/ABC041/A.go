c1947884f43e840b3caa74893386ef28078b151b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-12 10:14:52 +0900

    ABC041 A

---
+++ src/AtCoder/ABC041/A.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"fmt"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func main() {
+	s := readLine()
+	i := nextInt()
+
+	str := strings.Split(s, "")
+	fmt.Println(str[i-1])
+
+}
+
+func nextInt() int {
+	nextI, err := strconv.Atoi(readLine())
+	if err != nil {
+		panic(err)
+	}
+	return nextI
+}
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		line, isPrefix, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buf)
+}
+
