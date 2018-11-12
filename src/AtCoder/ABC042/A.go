ec18a62f5c197c0d4367c726a8f435d7d3481f0d
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-12 10:04:00 +0900

    ABC042 A

---
+++ src/AtCoder/ABC042/A.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func main() {
+	s := readLine()
+	s = strings.Replace(s, " ", "", -1)
+
+	if strings.Count(s, "7") == 1 && strings.Count(s, "5") == 2 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+
+}
+
+func nextInt() int {
+	sc.Scan()
+	nextI, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return nextI
+}
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		line, isPrefix, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buf)
+}
+
