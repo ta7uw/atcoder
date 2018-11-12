a72cccdcca3e2fe29eaadc89c08c78f74ced6c9a
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-13 00:31:46 +0900

    ABC032 A

---
+++ src/AtCoder/ABC032/A.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"fmt"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	X := lcm(a, b)
+	fmt.Println(((X+c-1))/X * X)
+
+}
+
+func gcd(x, y int) int {
+	if y == 0{
+		return x
+	}
+	return gcd(y, x % y)
+}
+
+func lcm(x, y int) int {
+	return x*y / gcd(x, y)
+}
+
+func nextInt() int {
+	sc.Scan()
+	nextI, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return nextI
+}
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		line, isPrefix, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buf)
+}
+
