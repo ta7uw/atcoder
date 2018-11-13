78e1d051c9020e3deec3cc5b43863500584d805b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-13 10:36:54 +0900

    ABC020 A

---
+++ src/AtCoder/ABC020/A.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"fmt"
+)
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func main() {
+	Q, _ := strconv.Atoi(readLine())
+	if Q == 1{
+		fmt.Println("ABC")
+	}else {
+		fmt.Println("chokudai")
+	}
+
+
+
+
+
+}
+
+func gcd(x, y int) int {
+	if y == 0{
+		return x
+	}
+	return gcd(y, x % y)
+}
+
+func lcm(x, y int) int {
+	return x*y / gcd(x, y)
+}
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		line, isPrefix, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buf)
+}
+
