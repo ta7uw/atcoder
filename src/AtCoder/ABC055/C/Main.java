db4ffbb9aa12f9d203b86b9f7c11fa7b34a256f7
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-18 14:30:54 +0900

    ABC055 C

---
+++ src/AtCoder/ABC055/C/Main.java
@@ -0,0 +1,145 @@
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner(System.in);
+        Task task = new Task();
+        task.solve(sc, out);
+        out.flush();
+        sc.close();
+    }
+
+    static class Task {
+        public void solve(Scanner sc, PrintWriter out) {
+            String n = nstr(sc);
+            BigDecimal N = new BigDecimal(n);
+            String m = nstr(sc);
+            BigDecimal M = new BigDecimal(m);
+            BigDecimal count = new BigDecimal("0");
+
+            BigDecimal tmp = M.subtract(N.multiply(new BigDecimal("2")));
+            if (tmp.compareTo(new BigDecimal("0")) >=0){
+                M = M.subtract(N.multiply(new BigDecimal("2")));
+                count = count.add(N);
+                count = count.add(M.divide(new BigDecimal("4"), 0, RoundingMode.DOWN));
+                out.println(count);
+            }else{
+                out.println(M.divide(new BigDecimal("2"), 0, RoundingMode.DOWN));
+            }
+
+
+        }
+    }
+
+
+    static int nint(Scanner sc) {
+
+
+        return Integer.parseInt(sc.next());
+    }
+
+    static long nlong(Scanner sc) {
+        return Long.parseLong(sc.next());
+    }
+
+    static double ndouble(Scanner sc) {
+        return Double.parseDouble(sc.next());
+    }
+
+    static float nfloat(Scanner sc) {
+        return Float.parseFloat(sc.next());
+    }
+
+    static String nstr(Scanner sc) {
+        return sc.next();
+    }
+
+    static long[] longLine(Scanner sc, int size) {
+        long[] lLine = new long[size];
+        for (int i = 0; i < size; i++) {
+            lLine[i] = nlong(sc);
+        }
+        return lLine;
+    }
+
+    static int[] intLine(Scanner sc, int size) {
+        int[] iLine = new int[size];
+        for (int i = 0; i < size; i++) {
+            iLine[i] = nint(sc);
+        }
+        return iLine;
+    }
+
+    static String[] strLine(Scanner sc, int size) {
+        String[] strLine = new String[size];
+        for (int i = 0; i < size; i++) {
+            strLine[i] = nstr(sc);
+        }
+        return strLine;
+    }
+
+    static long maxFromList(List<Long> longList) {
+        return longList.stream().max(Comparator.naturalOrder()).get();
+    }
+
+    static long minFromList(List<Long> longList) {
+        return longList.stream().min(Comparator.naturalOrder()).get();
+    }
+
+    public static int sumDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static long sumDigits(long n) {
+        long sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    static List<Integer> getIntegerList(Scanner sc, int size) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(nint(sc));
+        }
+        return list;
+    }
+
+    static List<Long> getLongList(Scanner sc, int size) {
+        List<Long> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(nlong(sc));
+        }
+        return list;
+    }
+
+    static List<String> getStringList(Scanner sc, int size){
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            stringList.add(nstr(sc));
+        }
+        return stringList;
+    }
+
+    private static long lcm(long a, long b) {
+        return a * b / gcd(b, a % b);
+    }
+
+    private static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
+
