82e129f18e17bdd35765f95a9ef744b82103afa8
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-25 08:30:15 +0900

    Participated in DPC5

---
+++ src/AtCoder/DPC5/A.py
@@ -0,0 +1,43 @@
+n = int(input())
+alIst = list(map(int, input().split()))
+ 
+sumN = 0
+ 
+for i in range(n):
+    sumN += alIst[i]
+ 
+mean = sumN / n
+ 
+min_margin = float("inf")
+frame = 0
+ 
+for i in range(n):
+    a = alIst[i]
+    margin = abs(a-mean)
+    if margin < min_margin:
+        min_margin = margin
+        frame = i
+ 
+print(frame)
+n = int(input())
+alIst = list(map(int, input().split()))
+
+sumN = 0
+
+for i in range(n):
+    sumN += alIst[i]
+
+mean = sumN / n
+
+min_margin = float("inf")
+frame = 0
+
+for i in range(n):
+    a = alIst[i]
+    margin = abs(a-mean)
+    if margin < min_margin:
+        min_margin = margin
+        frame = i
+
+print(frame)
+
