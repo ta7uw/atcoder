61c23279d8623ed0b9e425fd6edfd070c5c38846
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-11-25 16:57:08 +0900

    Modified: A.py

--- src/AtCoder/DPC5/A.py
+++ src/AtCoder/DPC5/A.py
@@ -19,25 +19,5 @@ for i in range(n):
         frame = i
  
 print(frame)
-n = int(input())
-alIst = list(map(int, input().split()))
 
-sumN = 0
-
-for i in range(n):
-    sumN += alIst[i]
-
-mean = sumN / n
-
-min_margin = float("inf")
-frame = 0
-
-for i in range(n):
-    a = alIst[i]
-    margin = abs(a-mean)
-    if margin < min_margin:
-        min_margin = margin
-        frame = i
-
-print(frame)
 
