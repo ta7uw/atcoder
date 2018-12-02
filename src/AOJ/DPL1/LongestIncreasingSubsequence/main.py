e79ad90215d14710dee022c7e0c9718b325b23b6
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-12-02 15:13:19 +0900

    Implemented: Longest Increasing Subsequence

---
+++ src/AOJ/DPL1/LongestIncreasingSubsequence/main.py
@@ -0,0 +1,21 @@
+import bisect
+
+n = int(input())
+a_list = [int(input()) for i in range(n)]
+
+
+def lis(n, A):
+    L = [100 for i in range(n + 1)]
+    L[0] = A[0]
+    length = 1
+    for i in range(1, n):
+        if L[length - 1] < A[i]:
+            L[length] = A[i]
+            length += 1
+        else:
+            index = bisect.bisect_left(L, A[i], 0, length - 1)
+            L[index] = A[i]
+    return length
+
+
+print(lis(n, a_list))
