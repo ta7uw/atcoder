245428002859756055d4daac3277f1d888c9152f
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-12-03 10:16:32 +0900

    ABC104 C

--- src/AtCoder/ABC104/C.py
+++ src/AtCoder/ABC104/C.py
@@ -1,45 +1,28 @@
 D, G = map(int, input().split())
 
-p_list = []
-c_list = []
-pm = {}
-cm = {}
+score_map = {}
 for i in range(D):
-    p, c = map(int, input().split())
-    p_list.append(p)
-    c_list.append(c)
-    pm[i] = p
-    pm[i] = c
+    score_map[i] = list(map(int, input().split()))
 
-p_sort = sorted(pm.items())
-print(p_sort)
-fi = 0
-co = 0
-result1=0
-for i in range(D-1, -1, -1):
-    if fi >= G:
-        break
-    p = p_list[i]
-    while p > 0:
-        p -= 1
-        fi += 100 * (i+1)
-        result1 +=1
-        if p==0:
-            fi += c_list[i]
-        if fi >= G:
-            break
-x = 0
-result2 = 0
-for k, p in p_sort.items():
-    x = 100*(k+1)*p + cm[k]
-    resutl2 += p
-    if x >= G:
-        break
-    
-    
-print(min([result1, result2])
+result_list = []
+for bit in range(1 << D):
+    sum = 0
+    result = 0
+    not_used = D
+    for i in range(D):
+        if 1 & bit >> i:
+            sum += score_map[i][1] + 100 * (i + 1) * score_map[i][0]
+            result += score_map[i][0]
+        else:
+            not_used = i
 
+    if not_used != D:
+        for p in range(score_map[not_used][0]):
+            if sum + (not_used + 1) * 100 * p >= G:
+                result_list.append(result + p)
 
+    else:
+        if sum >= G:
+            result_list.append(result)
 
-
-
+print(min(result_list))
