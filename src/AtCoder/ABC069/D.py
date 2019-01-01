879cf35220ce00e2dab881aa0d1d005084e22260
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-02 01:26:23 +0900

    ABC069 D

--- src/AtCoder/ABC069/D.py
+++ src/AtCoder/ABC069/D.py
@@ -2,27 +2,15 @@ h, w = map(int, input().split())
 n = int(input())
 a_list = list(map(int, input().split()))
 
-result = [[0 for _ in range(w)] for _ in range(h)]
-height = 0
-width = 0
-count = -1
+result = []
 
 for i in range(1, n + 1):
     a = a_list[i - 1]
     for j in range(a):
-        result[height][width] = i
-        count += 1
-        if width >= 0:
-            width += 1
-        else:
-            width -= 1
-        if count % w == w-1:
-            height += 1
-            if width >= 0:
-                width = -1
-            else:
-                width = 0
+        result.append(i)
 
-
-for r in result:
-    print(*r)
+for i in range(h):
+    if i & 1 == 0:
+        print(*result[i * w:(i + 1) * w])
+    else:
+        print(*result[i * w:(i + 1) * w][::-1])
