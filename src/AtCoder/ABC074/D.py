5736ec38759453b8da96a6bb53158b441c3ed54e
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-03 02:46:17 +0900

    ABC074 D

--- src/AtCoder/ABC074/D.py
+++ src/AtCoder/ABC074/D.py
@@ -1,13 +1,3 @@
-def warshall_floyd(graph):
-    num_v = len(graph)
-
-    for k in range(num_v):
-        for i in range(num_v):
-            for j in range(num_v):
-                if graph[i][j] > graph[i][k] + graph[k][j]:
-                    graph[i][j] = graph[i][k] + graph[k][j]
-    return graph
-
 N = int(input())
 graph = [[float("inf") for _ in range(N)] for _ in range(N)]
 for i in range(N):
@@ -16,20 +6,19 @@ for i in range(N):
         if i == j:
             continue
         graph[i][j] = a_list[j]
-import copy
-a_list = copy.deepcopy(graph)
-w_graph = warshall_floyd(graph)
+import numpy as np
+from scipy.sparse.csgraph import floyd_warshall
+w_graph = floyd_warshall(graph, directed=False)
+for i in range(N):
+    w_graph[i][i] = float("inf")
 result = 0
 for i in range(N):
-    for j in range(i+1, N):
-        if i == j:
-            continue
-        if w_graph[i][j] != a_list[i][j]:
+    w_graph[i][i] = float("inf")
+for i in range(N):
+    for j in range(i):
+        if w_graph[i][j] != graph[i][j]:
             print(-1)
             exit()
-        d = []
-        for k in range(N):
-            d.append(w_graph[i][k] + w_graph[j][k])
-        if min(d) > w_graph[i][j]:
+        if w_graph[i][j] < np.min(w_graph[i] + w_graph[j]):
             result += w_graph[i][j]
-print(result)
+print(int(result))
