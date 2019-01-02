0d899348e5b2183655d754851de3ac16d94d6406
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-02 16:41:55 +0900

    ABC073 D

--- src/AtCoder/ABC073/D.py
+++ src/AtCoder/ABC073/D.py
@@ -3,7 +3,8 @@ def washall_floyd(graph):
     for k in range(num_v):
         for i in range(num_v):
             for j in range(num_v):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
     return graph
 
 
