7d9774fa47b727b6bb1062ca75e8dd682147ad18
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-06 14:50:30 +0900

    ATC002 B

---
+++ src/AtCoder/ATC002/B.py
@@ -0,0 +1,20 @@
+N, M, P = map(int, input().split())
+
+
+def pow_mod(n, k, mod):
+    """
+    Exponentiation by squaring
+    :return: n ** k
+    """
+
+    if k == 0:
+        return 1
+
+    elif k & 1 == 1:
+        return pow_mod(n, k - 1, mod) * n % mod
+    else:
+        tmp = pow_mod(n, k // 2, mod)
+        return tmp * tmp % mod
+
+
+print(pow_mod(N, P, M))
