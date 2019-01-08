38062044aa84e3150943d3e5cb150d5c1a6618b9
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-08 22:37:58 +0900

    Add warshall floyd to mylib-python

--- mylib-python/warshall_floyd.py
+++ mylib-python/warshall_floyd.py
@@ -7,6 +7,17 @@ def warshall_floyd(graph):
                     graph[i][j] = graph[i][k] + graph[k][j]
     return graph
 
+
+def faster_warhsall_floyd(graph):
+    num_v = len(graph)
+    for i in range(num_v):
+        for j, c2 in enumerate(row[i] for row in graph):
+            for k, (c1, c3) in enumerate(zip(graph[j], graph[i])):
+                if c1 > c2 + c3:
+                    graph[j][k] = c2 + c3
+    return graph
+
+
 """
 from scipy.sparse.csgraph import floyd_warshall
 w_graph = floyd_warshall(graph, directed=False)
