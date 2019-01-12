39137d7f59d6d07a2ca5a121f6323258766988fa
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-12 13:27:31 +0900

    Update UnionFind in mylib-python

--- mylib-python/union_find.py
+++ mylib-python/union_find.py
@@ -2,6 +2,7 @@ class UnionFind:
     def __init__(self, n):
         self.parent = [i for i in range(n + 1)]
         self.rank = [0 for _ in range(n + 1)]
+        self.size = [1] * (n + 1)
 
     def find(self, x):
         # If x is root
@@ -15,14 +16,21 @@ class UnionFind:
     def union(self, x, y):
         x = self.find(x)
         y = self.find(y)
+        if x == y:
+            return
         # Make an edge from the root of lower tree to the root of higher tree
         if self.rank[x] < self.rank[y]:
             self.parent[x] = y
+            self.size[y] += self.size[x]
         else:
             self.parent[y] = x
+            self.size[x] += self.size[y]
             # If the height of tree the tree is the same, increase one of the heights by 1
             if self.rank[x] == self.rank[y]:
                 self.rank[x] += 1
 
     def check_same(self, x, y):
         return self.find(x) == self.find(y)
+
+    def get_size(self, x):
+        return self.size[self.find(x)]
