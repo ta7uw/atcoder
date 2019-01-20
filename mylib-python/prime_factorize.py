ddb1f7018d393ed49695e559ea261eaefaa1bec3
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-20 17:55:47 +0900

    Update prime_factorize in mylib-python

--- mylib-python/prime_factorize.py
+++ mylib-python/prime_factorize.py
@@ -19,3 +19,23 @@ def prime_factorize(num):
     if num > 1:
         prime_factor[num] = 1
     return prime_factor
+
+
+def divisorize(fct):
+    b, e = fct.pop()  # base, exponent
+    pre_div = divisorize(fct) if fct else [[]]
+    suf_div = [[(b, k)] for k in range(e + 1)]
+    return [pre + suf for pre in pre_div for suf in suf_div]
+
+
+def num(fct):
+    a = 1
+    for base, exponent in fct:
+        a = a * base ** exponent
+    return a
+
+# prime_factor = prime_factorize(N)
+# prime_factor = list(prime_factor.items())
+# prime_list = []
+# for div in divisorize(prime_factor):
+#     prime_list.append(num(div))
