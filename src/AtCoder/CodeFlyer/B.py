0eb17a01ee38c5a95d6eb543a1a92ef9bf9d40a3
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-01-27 12:58:04 +0900

    codeFlyer B

--- src/AtCoder/CodeFlyer/B.py
+++ src/AtCoder/CodeFlyer/B.py
@@ -7,14 +7,15 @@ items = []
 for _ in range(Q):
     items.append(tuple(map(int, input().split())))
 
-x_acc = list(accumulate(x_list)) + [0]
+x_acc = list(accumulate(x_list))
+x_acc = [0] + x_acc
 for item in items:
     c, d = item
     ans = 0
     i = bisect_left(x_list, c - d)
     j = bisect_left(x_list, c)
     k = bisect_left(x_list, c + d)
-    ans += c * (j - i) - (x_acc[j - 1] - x_acc[i - 1])
-    ans += x_acc[k - 1] - x_acc[j - 1] - c * (k - j)
+    ans += c * (j - i) - (x_acc[j] - x_acc[i])
+    ans += x_acc[k] - x_acc[j] - c * (k - j)
     ans += d * (N - k + i)
     print(ans)
