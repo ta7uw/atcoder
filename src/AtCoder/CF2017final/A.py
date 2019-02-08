0f7dd5960a8b4abaab1356c463cb6c70fccfdb54
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-02-08 23:32:31 +0900

    CF2017final A

---
+++ src/AtCoder/CF2017final/A.py
@@ -0,0 +1,9 @@
+S = input()
+T = ["AKIHABARA", "KIHABARA", "AKIHBARA", "AKIHABRA", "AKIHABAR",
+     "KIHBARA", "KIHABRA", "KIHABAR", "AKIHBRA", "AKIHBAR", "AKIHABR",
+     "KIHBRA", "KIHBAR", "KIHABR", "AKIHBR",
+     "KIHBR"]
+if S in T:
+    print("YES")
+else:
+    print("NO")
