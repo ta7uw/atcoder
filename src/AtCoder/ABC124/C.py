adae686fc0ca3c9a5c3b927509090219996ce19b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-04-13 22:55:58 +0900

    ABC124 C

--- src/AtCoder/ABC124/C.py
+++ src/AtCoder/ABC124/C.py
@@ -1,5 +1,28 @@
-A, B = map(int, input().split())
-if A >= B:
-    print(A + max(A - 1, B))
-else:
-    print(B + max(A, B - 1))
+S = input()
+
+n = len(S)
+
+start_0 = ""
+start_1 = ""
+
+for i in range(n):
+    if i % 2 == 0:
+        start_0 += "0"
+        start_1 += "1"
+    else:
+        start_0 += "1"
+        start_1 += "0"
+
+ans_0 = 0
+ans_1 = 0
+
+for i in range(n):
+    t1 = start_0[i]
+    t2 = start_1[i]
+    s = S[i]
+    if t1 != s:
+        ans_0 += 1
+    if t2 != s:
+        ans_1 += 1
+
+print(min(ans_1, ans_0))
