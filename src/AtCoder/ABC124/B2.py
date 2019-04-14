0566f302b4fff5a3074581eb818a8b08a072a60a
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-04-14 19:56:24 +0900

    ABC124 B2

--- src/AtCoder/ABC124/B2.py
+++ src/AtCoder/ABC124/B2.py
@@ -1,13 +1,11 @@
+from itertools import accumulate
+
 N = int(input())
 h_list = list(map(int, input().split()))
-from itertools import accumulate
 
 h_max = list(accumulate(h_list, max))
 ans = 0
-for i in range(N):
-    tmp = h_max[i]
-    h = h_list[i]
-    if tmp <= h:
+for h, m in zip(h_list, h_max):
+    if m <= h:
         ans += 1
-
 print(ans)
