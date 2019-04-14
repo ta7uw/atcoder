977affad925150d6270e1c87568496494d3338ee
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-04-14 17:42:20 +0900

    ABC124 D

--- src/AtCoder/ABC124/D.py
+++ src/AtCoder/ABC124/D.py
@@ -5,20 +5,19 @@ left = 0
 right = 0
 ans = 0
 remain = K
-S += "?"
 if S[0] == "0":
     remain -= 1
 
 while left < N:
     while right < N and remain >= 0:
-        if S[right] == "1" and S[right + 1] == "0":
+        if S[right] == "1" and right + 1 < N and S[right + 1] == "0":
             remain -= 1
         right += 1
     ans = max(ans, right - left)
     if left == right:
         right += 1
     else:
-        if S[left] == "0" and S[left + 1] == "1":
+        if S[left] == "0" and left + 1 < N and S[left + 1] == "1":
             remain += 1
     left += 1
 print(ans)
