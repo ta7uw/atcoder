69ec29ee2ed697792cc85b814e9c117b481967d2
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-04-18 00:17:44 +0900

    Codeforces Round552 Div3 B

--- src/CodeForces/Round552Div3/B.py
+++ src/CodeForces/Round552Div3/B.py
@@ -1,41 +1,34 @@
 N = int(input())
 a_list = list(map(int, input().split()))
 
-a_list.sort()
-min_a = a_list[0]
-max_a = a_list[-1]
+a_set = set(a_list)
+b_list = list(a_set)
+b_list.sort()
 
-cand = [0]
-if (max_a + min_a) % 2 == 0:
-    cand.append((max_a - min_a) // 2)
-cand.append(max_a - min_a)
-
-mean = a_list[N // 2]
-if N == 2:
-    for c in cand:
-        d = a_list[0] + c
-        d2 = a_list[-1] - c
-        if d == d2:
-            print(c)
-            exit()
+if len(b_list) > 3:
     print(-1)
-    exit()
-
-for c in cand:
-    tmp = []
+elif len(b_list) == 3:
+    mid = b_list[1]
+    if abs(mid - b_list[0]) == abs(mid - b_list[2]):
+        for a in a_list:
+            if a in b_list:
+                continue
+            else:
+                print(-1)
+                exit()
+        print(mid - b_list[0])
+    else:
+        print(-1)
+elif len(b_list) == 2:
+    if (b_list[1] - b_list[0]) % 2 == 0:
+        print((b_list[1] - b_list[0]) // 2)
+    else:
+        print(b_list[1] - b_list[0])
+else:
     for a in a_list:
-        if mean == a:
-            tmp.append(a)
-        elif mean > a:
-            tmp.append(a + c)
+        if b_list[0] == a:
+            continue
         else:
-            tmp.append(a - c)
-    at = tmp[0]
-    f = True
-    for t in tmp:
-        if t != at:
-            f = False
-    if f:
-        print(c)
-        exit()
-print(-1)
+            print(-1)
+            exit()
+    print(0)
