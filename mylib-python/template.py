68462fac1e72295ab7951c2561925de1696beadb
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-05-04 15:51:53 +0800

    Add template to mylib-python

---
+++ mylib-python/template.py
@@ -0,0 +1,21 @@
+import bisect
+import collections
+import copy
+import fractions
+import heapq
+import itertools
+import math
+import string
+import sys
+
+inf = float("inf")
+mod = 10 ** 9 + 7
+d4 = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+d8 = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
