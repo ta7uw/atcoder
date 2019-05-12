2415b02110f0de40c3032782561d0e8ac0c41e2f
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-05-12 15:57:25 +0900

    Fix divisors

--- mylib-python/divisors.py
+++ mylib-python/divisors.py
@@ -1,14 +1,12 @@
-def divisors(n):
+def divisors(num):
     """
     約数全列挙
-    :param n:
-    :return:
     """
     divisors = []
-    for i in range(1, int(n ** 0.5) + 1):
-        if n % i == 0:
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
             divisors.append(i)
-            if i != n // i:
-                divisors.append(n // i)
+            if i != num // i:
+                divisors.append(num // i)
 
     return divisors
