775a88549a927f37c6edc90a6db836207b245444
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-05-19 23:53:04 +0900

    ABC126 D

---
+++ src/AtCoder/ABC126/D.py
@@ -0,0 +1,34 @@
+N = int(input())
+import sys
+
+graph = [dict() for _ in range(N)]
+
+for i in range(N - 1):
+    u, v, w = map(int, sys.stdin.readline().split())
+    graph[u - 1][v - 1] = w
+    graph[v - 1][u - 1] = w
+
+color = [-1] * N
+
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
+
+def dfs(i, col):
+    color[i] = col
+    for k, v in graph[i].items():
+        if color[k] == -1:
+            if v % 2 == 0:
+                dfs(k, col)
+            else:
+                dfs(k, 1 - col)
+
+
+dfs(0, 1)
+
+# 1 -> 0
+# 2 -> 1
+
+for c in color:
+    print(c)
