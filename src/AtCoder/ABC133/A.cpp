d04cd3c3ffe4452176864b4254a389141943244a
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-15 14:30:16 +0900

    ABC133 A

---
+++ src/AtCoder/ABC133/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 1000000007;
+typedef pair<int, int> P;
+typedef long long ll;
+#define rep(i, n) for(int i=0;i<n;i++)
+#define rep2(i, m, n) for(int i=m;i<n;i++)
+#define rrep(i, n, m) for(int i=n;i>=m;i--)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+#ifdef __DEBUG
+#include "cpp-pyprint/pyprint.h"
+#endif
+
+void Main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+    cout << min(N*A, B) << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
