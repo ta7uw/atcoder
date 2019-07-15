26ed96deac1eeec187ebba0a67c4f6d4b0e30f43
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-15 10:42:42 +0900

    AGC035 A

--- src/AtCoder/AGC035/A.py
+++ src/AtCoder/AGC035/A.py
@@ -1,11 +1,21 @@
 N = int(input())
 a_list = list(map(int, input().split()))
 
-xor = 1
+tmp = set()
 for a in a_list:
-    xor ^= a
+    tmp.add(a)
 
-if xor == 1:
+if a_list.count(0) == N:
     print("Yes")
 else:
-    print("No")
+    if len(tmp) == 2 and a_list.count(0) == N // 3:
+        print("Yes")
+    elif len(tmp) == 3 and a_list.count(0) == 0:
+        b = list(tmp)
+        if b[0] ^ b[1] == b[2] and a_list.count(b[0]) == N // 3 \
+                and a_list.count(b[1]) == N // 3 and a_list.count(b[2]) == N // 3:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("No")
