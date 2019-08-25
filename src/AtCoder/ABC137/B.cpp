639b5b40a83d3a6eb9c9f4a43d7a7767865c320d
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-08-25 11:42:29 +0900

    ABC137 B

---
+++ src/AtCoder/ABC137/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> P;
+typedef long long ll;
+#define rep(i, n) for(int i=0;i<(n);i++)
+#define rep2(i, m, n) for(int i=m;i<(n);i++)
+#define rrep(i, n, m) for(int i=n;i>=(m);i--)
+using Graph = vector<vector<int>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const ll MOD = 1000000007;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int K, X;
+    cin >> K >> X;
+
+    for (int i = X - K + 1; i <= K + X - 1; ++i) {
+        cout << i << " ";
+    }
+    cout << "" << endl;
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
