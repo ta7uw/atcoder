537a5187b24a3bebadedd344032cd48e2a20dd2f
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-01 15:04:50 +0900

    ABC077 C

---
+++ src/AtCoder/ABC077/C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N;
+    cin >> N;
+    vector<ll> A(N), B(N), C(N);
+    rep(i, N) cin >> A[i];
+    rep(i, N) cin >> B[i];
+    rep(i, N) cin >> C[i];
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    sort(C.begin(), C.end());
+    ll ans = 0;
+    rep(i, N) {
+        ll b = B[i];
+        ll cnt = upper_bound(A.begin(), A.end(), b - 1) - A.begin();
+        ll cnt2 = N - (lower_bound(C.begin(), C.end(), b + 1) - C.begin());
+        ans += cnt * cnt2;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
