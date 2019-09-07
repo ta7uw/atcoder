52991d203017e2aa0207d40a88e28c16a65588e1
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-07 20:41:38 +0900

    EDPC B cpp

---
+++ src/AtCoder/EDPC/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N, K;
+    cin >> N >> K;
+    vector<ll> H(N);
+    rep(i, N) cin >> H[i];
+    vector<ll> dp(N + K + 1, INF);
+    dp[0] = 0;
+    rep(i, N) {
+        rep2(k, 1, K + 1) {
+            dp[i + k] = min(dp[i] + abs(H[i + k] - H[i]), dp[i + k]);
+        }
+    }
+    cout << dp[N - 1] << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
