9b9df94281e97b6a4c9b4cf8a1025b78942d1f71
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-07 20:34:11 +0900

    EducationalRound72 Div2 A

---
+++ src/CodeForces/EducationalRound72/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int T;
+    cin >> T;
+    rep(i, T) {
+        ll s, in, e;
+        cin >> s >> in >> e;
+        if (s + e <= in) {
+            cout << 0 << endl;
+        } else {
+            ll tmp = (s + e - in + 1) / 2;
+            if (e == 0) {
+                cout << 1 << endl;
+            }else{
+                cout << min(e + 1, tmp) << endl;
+            }
+        }
+    }
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
