19f5dceed2ea01d3c0a85ef358f755e1eb94b250
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-07 20:39:41 +0900

    ABC124 D cpp

---
+++ src/AtCoder/ABC124/D.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N, K;
+    cin >> N >> K;
+    string S;
+    cin >> S;
+
+    int ans = 0;
+    int left = 0;
+    int right = 0;
+    int remain = K;
+    if (S[0] == '0') {
+        remain--;
+    }
+    while (left < N) {
+        while (remain >= 0 && right < N) {
+            if (S[right] == '1' && right + 1 < N && S[right + 1] == '0') {
+                remain--;
+            }
+            right++;
+        }
+        ans = max(ans, right - left);
+        if (right == left) {
+            right++;
+        }
+        if (S[left] == '0' && left + 1 >= 0 && S[left + 1] == '1') {
+            remain++;
+        }
+
+        left++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
