446dae56694b5ccee298c37c1cefcd71cf351cd7
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-08 16:20:58 +0900

    ABC140 B

---
+++ src/AtCoder/ABC140/B.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N), C(N - 1);
+    rep(i, N) cin >> A[i];
+    rep(i, N) cin >> B[i];
+    rep(i, N - 1) cin >> C[i];
+
+    ll ans = accumulate(B.begin(), B.end(), 0LL);
+    rep(i, N - 1) {
+        if (A[i] + 1 == A[i + 1]) {
+            ans += C[A[i]-1];
+        }
+    }
+    cout << ans << endl;
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
