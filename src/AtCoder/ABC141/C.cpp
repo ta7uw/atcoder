5989984cf6c0e0252209d3740efa270e6c96d337
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-16 01:21:25 +0900

    ABC141 C

---
+++ src/AtCoder/ABC141/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N, K, Q;
+    cin >> N >> K >> Q;
+    unordered_map<int, int> cnt;
+    rep(i, Q) {
+        int a;
+        cin >> a;
+        a--;
+        cnt[a]++;
+    }
+
+    rep(i, N) {
+        int t = K - Q + cnt[i];
+        if (t > 0) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
