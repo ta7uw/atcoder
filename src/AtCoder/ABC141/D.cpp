f000ba9d1b6f1d6f0828d4e161100da8e75311fa
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-16 01:21:39 +0900

    ABC141 D

---
+++ src/AtCoder/ABC141/D.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using edge = pair<ll, P>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+using Graph = vector<vector<ll>>;
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int N, M;
+    cin >> N >> M;
+    priority_queue<int> que;
+    rep(i, N) {
+        int a;
+        cin >> a;
+        que.push(a);
+    }
+
+    while (M > 0) {
+        M--;
+        int q = que.top(); que.pop();
+        q /= 2;
+        que.push(q);
+    }
+
+    ll ans = 0;
+    while (que.size() != 0) {
+        int q = que.top(); que.pop();
+        ans += q;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
