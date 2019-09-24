c82fa4b108a0831c9cc6a5318399d28157f756dc
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-09-24 23:25:14 +0900

    codeforces Round 588 Div 2 B

---
+++ src/CodeForces/Round588Div2/B.py
@@ -0,0 +1,27 @@
+"""
+Kが残っているときに i 桁目が0以外であれば0に置き換える。
+ただし、先頭は0にはできないことに注意する。
+1桁のときは0にできるので別処理にする。
+"""
+N, K = map(int, input().split())
+S = input()
+
+if N == 1 and K >= 1:
+    print(0)
+    exit()
+
+T = ""
+for i, s in enumerate(S):
+    if i == 0:
+        if K >= 1 and s != "1":
+            K -= 1
+            T += "1"
+        else:
+            T += s
+    else:
+        if K >= 1 and s != "0":
+            K -= 1
+            T += "0"
+        else:
+            T += s
+print(T)
