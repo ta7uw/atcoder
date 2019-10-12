a2c0707a8adcd8b69d0dfa4fc3bf3714cf8b2f1a
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-10-12 19:31:43 +0900

    ABC142 C

---
+++ src/AtCoder/ABC142/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+/**
+ * C - Go to School
+ * https://atcoder.jp/contests/abc142/tasks/abc142_c
+ * indexをもたせて、A が小さい順にソートすると早く来た人がわかる
+ */
+void Main() {
+    int N;
+    cin >> N;
+    vector<P> A(N);
+    rep(i, N) {
+        int a;
+        cin >> a;
+        A[i] = {a, i + 1};
+    }
+    sort(A.begin(), A.end());
+    for (P p : A) {
+        cout << p.second << '\n';
+    }
+
+
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+}
