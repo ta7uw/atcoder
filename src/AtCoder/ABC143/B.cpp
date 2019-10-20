ac1b61efc8b735c2d4b034139c1af2257a51b608
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-10-20 13:18:33 +0900

    ABC143 B

---
+++ src/AtCoder/ABC143/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+/**
+ * B - TAKOYAKI FESTIVAL 2019
+ * https://atcoder.jp/contests/abc143/tasks/abc143_b
+ *
+ * 同じものを二回数えないようにしてループを回す
+ */
+void Main() {
+    int N;
+    cin >> N;
+    vector<int> D(N);
+    rep(i, N) cin >> D[i];
+    int ans = 0;
+    rep(i, N) {
+        rep2(j, i + 1, N) {
+            ans += D[i] * D[j];
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
