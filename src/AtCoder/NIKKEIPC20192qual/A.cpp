f138f2148a481c162323124ad77ffa9d3f585fb9
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-11-10 00:27:13 +0900

    NIKKEIPC 2019 2 qual A

---
+++ src/AtCoder/NIKKEIPC20192qual/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+/**
+ * A - Sum of Two Integers
+ * https://atcoder.jp/contests/nikkei2019-2-qual/tasks/nikkei2019_2_qual_a
+ * N が偶数の時 N / 2 - 1, N が奇数の時 (N - 1) / 2
+ * (N - 1) / 2 を出力すればよい
+ */
+void Main() {
+    int N;
+    cin >> N;
+    cout << (N - 1) / 2 << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
