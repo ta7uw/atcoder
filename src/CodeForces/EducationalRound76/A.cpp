4b22d2cbdb5242b61f6f003afbb1043fab9586a6
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-11-14 23:57:00 +0900

    Codeforces Educational Round Div2 A

---
+++ src/CodeForces/EducationalRound76/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+/**
+ * A. Two Rival Students
+ * https://codeforces.com/contest/1257/problem/A
+ *
+ * 最大でも n - 1 までしか距離をとれないので　min(abs(a - b) + x, n - 1) が答え
+ */
+void Main() {
+    int T;
+    cin >> T;
+    rep(t, T) {
+        int n, x, a, b;
+        cin >> n >> x >> a >> b;
+        cout << min(abs(a - b) + x, n - 1) << '\n';
+    }
+
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
