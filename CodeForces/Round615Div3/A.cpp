6cc65f210044b660951232826c05e699da40ecf9
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-01-26 17:32:53 +0900

    Codeforces Round615 A

---
+++ CodeForces/Round615Div3/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    int T;
+    cin >> T;
+    rep(t, T) {
+        ll n;
+        vector<ll> A(3);
+        rep(i, 3) cin >> A[i];
+        cin >> n;
+        sort(A.begin(), A.end());
+        ll tmp = A[2] - A[0] + A[2] - A[1];
+        n -= tmp;
+        if (n >= 0 && n % 3 == 0) {
+            cout << "YES" << '\n';
+        } else {
+            cout << "NO" << '\n';
+        }
+    }
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
