0cc64ef0dcba01cee68799886bbe1364167bb671
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-01-26 23:36:53 +0900

    ABC153 E

---
+++ AtCoder/ABC153/E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll H, N;
+    cin >> H >> N;
+    vector<P> items(N);
+    rep(i, N) cin >> items[i].first >> items[i].second;
+    vector<ll> dp(H + 1, INF);
+    dp[H] = 0;
+    rrep(i, H, 0) {
+        rep(j, N) {
+            P p = items[j];
+            ll m = max(0LL, i-p.first);
+            dp[m] = min(dp[m], dp[i] + p.second);
+        }
+    }
+    cout << dp[0] << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
