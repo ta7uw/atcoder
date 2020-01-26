474d093142bfa927dcf42bc935817de2f58a50b4
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-01-26 17:46:05 +0900

    Codeforces Round615 D

---
+++ CodeForces/Round615Div3/D.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll Q, x;
+    cin >> Q >> x;
+    vector<ll> Y(Q);
+    rep(i, Q) cin >> Y[i];
+    ll mex = 0;
+    vector<ll> ans;
+    map<ll, ll> cnt;
+    rep(i, Q) {
+        ll y = Y[i];
+        if (y == mex || abs(y - mex) % x == 0) {
+            mex++;
+            while (cnt[mex % x] >= 1) {
+                cnt[mex % x]--;
+                mex++;
+            }
+        } else {
+            cnt[y % x] ++;
+        }
+        ans.push_back(mex);
+    }
+    rep(i, Q) cout << ans[i] << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
