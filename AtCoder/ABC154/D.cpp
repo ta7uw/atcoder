5ae942de82fba6783429a631fb0c263b6ab0ac6b
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-02-16 11:55:11 +0900

    ABC154 D

---
+++ AtCoder/ABC154/D.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll N, K;
+    cin >> N >> K;
+    vector<double> P(N);
+    rep(i, N) cin >> P[i];
+    vector<double> acc(N + 1, 0);
+    rep(i, N) {
+        acc[i + 1] += acc[i] + (1 + P[i]) / 2.0;
+    }
+    double ans = 0;
+    rep2(i, K, N + 1) {
+        ans = max(ans, acc[i] - acc[i - K]);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
