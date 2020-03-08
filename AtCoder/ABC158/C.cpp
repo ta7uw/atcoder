f56e7992d53bb5f41703f9e62cdb7d5ae7d6fe12
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-03-08 17:39:12 +0900

    ABC158 C

---
+++ AtCoder/ABC158/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+#ifdef __DEBUG
+
+/**
+ * For DEBUG
+ * https://github.com/ta7uw/cpp-pyprint
+ */
+#include "cpp-pyprint/pyprint.h"
+
+#endif
+
+void Main() {
+    ll A, B;
+    cin >> A >> B;
+    rep2(i, 1, 10000) {
+        ll a = 0.08 * i;
+        ll b = 0.1 * i;
+        if (A==a && b ==B) {
+            cout << i << '\n';
+            return;
+        }
+    }
+    cout << -1 << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
